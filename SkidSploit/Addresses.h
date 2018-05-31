3feffe1cf34122a0b7d9990bc48586aec47d4ab8
elkizza <39752208+elkizza@users.noreply.github.com>
2018-05-31 01:22:19 +0100

    Updated for roblox 31/05/2018

--- SkidSploit/Addresses.h
+++ SkidSploit/Addresses.h
@@ -11,20 +11,20 @@ int ScriptContext = 0;
 int LuaState = 0;
 
 enum rbxOffsets {
-	SCRIPTCONTEXT = 0x1121698,// Updated
-	PENVCHECK = 0x7385f7, // Updated
-	LUA_GETFIELD = 0x73F4E0, // Updated
-	LUA_SETFIELD = 0x741290, // Updated
-	LUA_SETTOP = 0x741660, // Updated
-	LUA_PUSHSTRING = 0x740880, // Updated
-	LUA_PUSHNUMBER = 0x7407F0, // Updated
-	LUA_PUSHVALUE = 0x740950, // Updated
-	LUA_GETMETATABLE = 0x73F7B0, // Updated
-	LUA_PCALL = 0x7402C0, // Updated
+	SCRIPTCONTEXT = 0x1128D10,// Updated
+	PENVCHECK = 0x7355d7, // Updated
+	LUA_GETFIELD = 0x73C460, // Updated
+	LUA_SETFIELD = 0x73E230, // Updated
+	LUA_SETTOP = 0x73E610, // Updated
+	LUA_PUSHSTRING = 0x73D830, // Updated
+	LUA_PUSHNUMBER = 0x73d7a0, // Updated
+	LUA_PUSHVALUE = 0x73D8F0, // Updated
+	LUA_GETMETATABLE = 0x73C730, // Updated
+	LUA_PCALL = 0x73d250, // Updated
 };
 
 int GlobalState(DWORD ScriptContext) {
-	return(ScriptContext + 56 * 0 + 164) ^ *(DWORD *)(ScriptContext + 56 * 0 + 164);
+	return *(DWORD *)(ScriptContext + 56 * 1 + 164) - (ScriptContext + 56 * 1 + 164);
 }
 
 //LuaState = (DontUpdateMe + 56 * DontUpdateMePl0x + 164) ^ *(DWORD *)(DontUpdateMe + 56 *  + 164);

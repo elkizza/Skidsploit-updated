0c1b9b8b94c755fafc42b70028eb05aecf14c311
elkizza <39752208+elkizza@users.noreply.github.com>
2018-10-09 13:38:29 +0100

    Updated Addresses.

--- SkidSploit/Addresses.h
+++ SkidSploit/Addresses.h
@@ -11,16 +11,16 @@ int ScriptContext = 0;
 int LuaState = 0;
 
 enum rbxOffsets {
-	SCRIPTCONTEXT = 0x1169688,// Updated
-	PENVCHECK = 0x723a87, // Updated
-	LUA_GETFIELD = 0x72A970, // Updated
-	LUA_SETFIELD = 0x72C6A0, // Updated
-	LUA_SETTOP = 0x72ca70, // Updated
-	LUA_PUSHSTRING = 0x72BCD0, // Updated
-	LUA_PUSHNUMBER = 0x72bc40, // Updated
-	LUA_PUSHVALUE = 0x72BD90, // Updated
-	LUA_GETMETATABLE = 0x72AC30, // Updated
-	LUA_PCALL = 0x72B710, // Updated
+	SCRIPTCONTEXT = 0x1397508,// Updated
+	PENVCHECK = 0x774ce7, // Updated
+	LUA_GETFIELD = 0x77BC20, // Updated
+	LUA_SETFIELD = 0x77D9C0, // Updated
+	LUA_SETTOP = 0x77dd90, // Updated
+	LUA_PUSHSTRING = 0x77CFD0, // Updated
+	LUA_PUSHNUMBER = 0x77cf40, // Updated
+	LUA_PUSHVALUE = 0x77D0A0, // Updated
+	LUA_GETMETATABLE = 0x77BEF0, // Updated
+	LUA_PCALL = 0x77C9F0, // Updated
 };
 
 int GlobalState(DWORD ScriptContext) {

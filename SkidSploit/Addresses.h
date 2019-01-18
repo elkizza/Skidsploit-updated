4772b2ad393d2af8c673f5b1ca29f27f8f3e0522
elkizza <39752208+elkizza@users.noreply.github.com>
2019-01-18 16:53:48 +0000

    Updated
    Updated addresses Changelog: 
    penvcheck is no more!

--- SkidSploit/Addresses.h
+++ SkidSploit/Addresses.h
@@ -11,20 +11,23 @@ int ScriptContext = 0;
 int LuaState = 0;
 
 enum rbxOffsets {
-	SCRIPTCONTEXT = 0x1397508,// Updated
-	PENVCHECK = 0x774ce7, // Updated
-	LUA_GETFIELD = 0x77BC20, // Updated
-	LUA_SETFIELD = 0x77D9C0, // Updated
-	LUA_SETTOP = 0x77dd90, // Updated
-	LUA_PUSHSTRING = 0x77CFD0, // Updated
-	LUA_PUSHNUMBER = 0x77cf40, // Updated
-	LUA_PUSHVALUE = 0x77D0A0, // Updated
-	LUA_GETMETATABLE = 0x77BEF0, // Updated
-	LUA_PCALL = 0x77C9F0, // Updated
+	SCRIPTCONTEXT = 0x17BB038,// Updated 18/01/2019
+	//PENVCHECK = 0x774ce7, // Updated Not needed anymore due to roblox removing pcall's check
+	LUA_GETFIELD = 0x77cc70, // Updated 18/01/2019
+	LUA_SETFIELD = 0x77ea20, // Updated 18/01/2019
+	LUA_SETTOP = 0x77edf0, // Updated 18/01/2019
+	LUA_PUSHSTRING = 0x77e010, // Updated 18/01/2019
+	LUA_PUSHNUMBER = 0x77df80, // Updated 18/01/2019
+	LUA_PUSHVALUE = 0x77e0c0, // Updated 18/01/2019
+	LUA_GETMETATABLE = 0x77cf40, // Updated 18/01/2019
+	LUA_PCALL = 0x77da50, // Updated 18/01/2019
 };
 
+
 int GlobalState(DWORD ScriptContext) {
-	return (ScriptContext + 56 * 1 + 164) + *(DWORD *)(ScriptContext + 56 * 1 + 164);
+	int v39 = ScriptContext; // updated
+        int v51 = 0; // updated 
+	return v39 + 56 * v51 + 164 + *(DWORD *)(v39 + 56 * v51 + 164); // updated
 }
 
 //LuaState = (DontUpdateMe + 56 * DontUpdateMePl0x + 164) ^ *(DWORD *)(DontUpdateMe + 56 *  + 164);
